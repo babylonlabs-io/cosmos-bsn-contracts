96bd4395c74a1d4766fa7a8e009e4773af4716fb
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2025-06-26 07:58:46 +0800

    Construct init message only when necessary
    Currently the btc_lc_message is constructed unconditionally, which is
    not necessary.

--- contracts/babylon/src/contract.rs
+++ contracts/babylon/src/contract.rs
@@ -55,18 +55,21 @@ pub fn instantiate(
     // instantiate btc light client contract first
     // It has to be before btc staking and finality contracts which depend on it
     if let Some(btc_light_client_code_id) = msg.btc_light_client_code_id {
-        let btc_lc_init_msg = BtcLightClientInstantiateMsg {
-            network: msg.network.clone(),
-            btc_confirmation_depth: msg.btc_confirmation_depth,
-            checkpoint_finalization_timeout: msg.checkpoint_finalization_timeout,
+        let init_msg = match msg.btc_light_client_msg {
+            Some(lc_msg) => lc_msg,
+            None => {
+                let btc_lc_init_msg = BtcLightClientInstantiateMsg {
+                    network: msg.network.clone(),
+                    btc_confirmation_depth: msg.btc_confirmation_depth,
+                    checkpoint_finalization_timeout: msg.checkpoint_finalization_timeout,
+                };
+                to_json_binary(&btc_lc_init_msg)?
+            }
         };
-        // Instantiate BTC light client contract first
         let init_msg = WasmMsg::Instantiate {
             admin: msg.admin.clone(),
             code_id: btc_light_client_code_id,
-            msg: msg
-                .btc_light_client_msg
-                .unwrap_or(to_json_binary(&btc_lc_init_msg)?),
+            msg: init_msg,
             funds: vec![],
             label: "BTC Light Client".into(),
         };
