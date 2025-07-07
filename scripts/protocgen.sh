a8f69f97d407284b3b1b5caf2931fa3f9f54fd4a
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2025-07-07 08:20:48 +0800

    Remove `packages/proto/buf.yaml`

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
-# This script generates protobuf messages in Rust for the Wasm smart contract
-# NOTE: use `cargo run-script gen-proto` in the root dir instead.
+# This script generates protobuf messages in Rust using a Buf workspace
+# Use with: `cargo run-script gen-proto` in the root dir.
 
 set -eo pipefail
 
-cd packages/proto
-# To also initialize, fetch and checkout any nested submodules
+PROTO_ROOT="packages/proto"
+
+cd "$(dirname "$0")/../"
+
+# Initialize git submodules
 git submodule update --init --recursive
-# Update the buf.yaml file to include the referenced branch
-buf mod update
-# Generate the Rust protobuf messages
-buf generate --template buf.gen.rust.yaml
-cd ../..
+
+# Run `buf dep update` in the submodule(s)
+echo "🔄 Updating buf deps in submodules..."
+(cd "$PROTO_ROOT/babylon/proto" && buf dep update)
+
+# Run `buf generate` from the workspace root
+echo "⚙️ Generating Rust protobuf bindings..."
+(cd "$PROTO_ROOT" && buf generate --template buf.gen.rust.yaml)
+
+echo "✅ Done generating protobuf bindings."
