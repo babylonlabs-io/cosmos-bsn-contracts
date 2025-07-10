1d3c85fe4452d456515487dfd5a852545ff67f86
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2025-07-10 18:19:50 +0800

    feat(btc-light-client): add median time past check
    This commit introduces the median time past (MTP) check as defined in BIP 113. The MTP is the median of the timestamps of the last 11 blocks. A block is only considered valid if its timestamp is strictly greater than the MTP of the preceding block.
    
    This change enhances the security of the BTC light client by preventing miners from manipulating timestamps to their advantage.

--- contracts/btc-light-client/src/bitcoin.rs
+++ contracts/btc-light-client/src/bitcoin.rs
@@ -7,13 +7,18 @@
 //!
 //! It mirrors the logic used in Bitcoin Core and Babylon's Go implementation.
 
-use crate::state::get_header;
+use crate::state::{get_base_header, get_header, get_header_by_hash};
 use babylon_bitcoin::{deserialize, BlockHeader, Work};
 use babylon_bitcoin::{Params, Uint256};
 use babylon_proto::babylon::btclightclient::v1::BtcHeaderInfo;
-use bitcoin::{BlockHash, Target};
+use bitcoin::hashes::Hash;
+use bitcoin::{BlockHash, Network, Target};
 use cosmwasm_std::Storage;
 use cosmwasm_std::{StdError, StdResult};
+use std::collections::BTreeMap;
+
+/// bip-0113 defines the median of the last 11 blocks instead of the block's timestamp for lock-time calculations.
+const MEDIAN_TIME_SPAN: usize = 11;
 
 /// Errors that can occur during BTC header verification.
 #[derive(thiserror::Error, Debug, PartialEq)]
@@ -39,8 +44,15 @@ pub enum HeaderError {
     #[error("Header's prev_blockhash {got} does not match parent header's hash {expected}")]
     PrevHashMismatch { got: BlockHash, expected: BlockHash },
 
+    /// Block's timestamp is too old.
+    #[error("Time is the median time of last 11 blocks or before")]
+    TimeTooOld,
+
     #[error("Failed to decode BTC header: {0}")]
     DecodeError(String),
+
+    #[error("Header {0} missing from store and pending")]
+    MissingHeader(BlockHash),
 
     #[error(transparent)]
     Std(#[from] StdError),
@@ -67,6 +79,12 @@ pub fn verify_headers(
     let mut last_header = first_header.clone();
     let mut cum_work_old = total_work(last_header.work.as_ref())?;
 
+    // A collection of headers that have been verified but not yet committed to storage.
+    // This is necessary for the `calculate_median_time_past` function, which needs
+    // to look at the last 11 headers. Some of those headers might be in the
+    // `new_headers` slice, which is not yet in storage.
+    let mut pending_headers = BTreeMap::new();
+
     for (i, new_header) in new_headers.iter().enumerate() {
         let prev_block_header: BlockHeader = deserialize(last_header.header.as_ref())?;
         let block_header: BlockHeader = deserialize(new_header.header.as_ref())?;
@@ -85,6 +103,7 @@ pub fn verify_headers(
             last_header.height,
             &prev_block_header,
             &block_header,
+            &pending_headers,
         )?;
 
         let cum_work = total_work(new_header.work.as_ref())?;
@@ -107,6 +126,7 @@ pub fn verify_headers(
         // this header is good, verify the next one
         cum_work_old = cum_work;
         last_header = new_header.clone();
+        pending_headers.insert(block_header.block_hash(), block_header);
     }
     Ok(())
 }
@@ -120,6 +140,7 @@ fn check_header(
     prev_block_height: u32,
     prev_block_header: &BlockHeader,
     header: &BlockHeader,
+    pending_headers: &BTreeMap<BlockHash, BlockHeader>,
 ) -> Result<(), HeaderError> {
     check_block_header_context(
         storage,
@@ -127,6 +148,7 @@ fn check_header(
         prev_block_height,
         prev_block_header,
         header,
+        pending_headers,
     )?;
 
     // Perform proof-of-work check as specified in:
@@ -175,6 +197,7 @@ fn check_block_header_context(
     prev_block_height: u32,
     prev_block_header: &BlockHeader,
     header: &BlockHeader,
+    pending_headers: &BTreeMap<BlockHash, BlockHeader>,
 ) -> Result<(), HeaderError> {
     let expected_target =
         get_next_work_required(storage, prev_block_height, prev_block_header, chain_params)?;
@@ -191,7 +214,68 @@ fn check_block_header_context(
         });
     }
 
+    // BIP 113
+    let height = prev_block_height + 1;
+
+    let csv_height = match chain_params.network {
+        Network::Bitcoin => 419328, // 000000000000000004a1b34462cb8aeebd5799177f7a29cf28f2d1961716b5b5
+        Network::Testnet => 770112, // 00000000025e930139bac5c6c31a403776da130831ab85be56578f3fa75369bb
+        Network::Signet => 1,       // Always active unless overridden
+        Network::Regtest => 1,      // Always active unless overridden
+        _ => unreachable!("Unsupported network"),
+    };
+
+    let base_height = get_base_header(storage)?.height;
+
+    if height >= base_height + MEDIAN_TIME_SPAN as u32 && height >= csv_height {
+        let mtp = calculate_median_time_past(storage, header, pending_headers)?;
+        if header.time <= mtp {
+            return Err(HeaderError::TimeTooOld);
+        }
+    }
+
     Ok(())
+}
+
+/// Calculates the median time of the previous few blocks prior to the header (inclusive).
+fn calculate_median_time_past(
+    storage: &dyn Storage,
+    header: &BlockHeader,
+    pending_headers: &BTreeMap<BlockHash, BlockHeader>,
+) -> Result<u32, HeaderError> {
+    let mut timestamps = Vec::with_capacity(MEDIAN_TIME_SPAN);
+
+    timestamps.push(header.time);
+
+    let zero_hash = BlockHash::all_zeros();
+
+    let mut block_hash = header.prev_blockhash;
+
+    while timestamps.len() < MEDIAN_TIME_SPAN {
+        // Genesis block
+        if block_hash == zero_hash {
+            break;
+        }
+
+        let header: BlockHeader = match get_header_by_hash(storage, block_hash.as_ref()) {
+            Ok(raw_header) => deserialize(raw_header.header.as_ref())?,
+            Err(_) => pending_headers
+                .get(&block_hash)
+                .cloned()
+                .ok_or(HeaderError::MissingHeader(block_hash))?,
+        };
+
+        timestamps.push(header.time);
+
+        block_hash = header.prev_blockhash;
+    }
+
+    timestamps.sort_unstable();
+
+    Ok(timestamps
+        .get(timestamps.len() / 2)
+        .copied()
+        .expect("Timestamps must be non-empty; qed"))
 }
 
 /// Usually, it's just the target of last block. However, if we are in a retarget period,
