b253b31edd5035e731cb761cfdfa8ac69aa5a60f
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2025-07-15 17:36:06 +0800

    Remove broken tests

--- contracts/btc-light-client/tests/integration.rs
+++ contracts/btc-light-client/tests/integration.rs
@@ -17,16 +17,10 @@
 //!      });
 //! 4. Anywhere you see query(&deps, ...) you must replace it with query(&mut deps, ...)
 
-use babylon_test_utils::{get_btc_lc_fork_msg, get_btc_lc_mainchain_resp};
-use btc_light_client::msg::btc_header::{BtcHeader, BtcHeaderResponse};
-use btc_light_client::msg::contract::{ExecuteMsg, InstantiateMsg};
-use cosmwasm_std::testing::{message_info, mock_ibc_channel_open_try};
-use cosmwasm_std::{from_json, Addr, ContractResult, IbcOrder, Response};
-use cosmwasm_vm::testing::{
-    execute, ibc_channel_open, instantiate, mock_env, mock_info, mock_instance,
-    mock_instance_with_gas_limit, query, MockApi, MockQuerier, MockStorage,
-};
-use cosmwasm_vm::Instance;
+use btc_light_client::msg::contract::InstantiateMsg;
+use cosmwasm_std::testing::message_info;
+use cosmwasm_std::{Addr, ContractResult, Response};
+use cosmwasm_vm::testing::{instantiate, mock_env, mock_instance};
 
 static BABYLON_CONTRACT_WASM: &[u8] = include_bytes!("../../../artifacts/btc_light_client.wasm");
 
@@ -34,21 +28,6 @@ static BABYLON_CONTRACT_WASM: &[u8] = include_bytes!("../../../artifacts/btc_lig
 const MAX_WASM_SIZE: usize = 1024 * 1024; // 1 MB
 
 const CREATOR: &str = "creator";
-
-#[track_caller]
-fn setup() -> Instance<MockApi, MockStorage, MockQuerier> {
-    let mut deps = mock_instance_with_gas_limit(BABYLON_CONTRACT_WASM, 2_250_000_000_000);
-    let msg = InstantiateMsg {
-        network: btc_light_client::BitcoinNetwork::Regtest,
-        btc_confirmation_depth: 10,
-        checkpoint_finalization_timeout: 99,
-        initial_header: babylon_test_utils::initial_header(),
-    };
-    let info = message_info(&Addr::unchecked(CREATOR), &[]);
-    let res: Response = instantiate(&mut deps, mock_env(), info, msg).unwrap();
-    assert_eq!(0, res.messages.len());
-    deps
-}
 
 #[test]
 fn wasm_size_limit_check() {
@@ -75,73 +54,3 @@ fn instantiate_works() {
     let msgs = res.unwrap().messages;
     assert_eq!(0, msgs.len());
 }
-
-/*
-#[test]
-fn btc_headers_works() {
-    let mut deps = setup();
-    let env = mock_env();
-    let info = mock_info("anyone", &[]);
-
-    let test_headers = get_main_msg_test_headers();
-
-    let execute_msg = ExecuteMsg::BtcHeaders {
-        headers: test_headers,
-    };
-
-    execute::<_, _, _, _, BabylonMsg>(&mut deps, env, info, execute_msg).unwrap();
-}
-
-#[test]
-fn btc_headers_fork_works() {
-    let mut deps = setup();
-    let env = mock_env();
-    let info = mock_info("anyone", &[]);
-
-    // Initialization
-    let test_headers = get_main_msg_test_headers();
-
-    let execute_msg = ExecuteMsg::BtcHeaders {
-        headers: test_headers,
-    };
-
-    execute::<_, _, _, _, BabylonMsg>(&mut deps, env.clone(), info.clone(), execute_msg).unwrap();
-
-    // Fork / continuation
-    let test_headers = get_fork_msg_test_headers();
-
-    let execute_msg = babylon_contract::msg::contract::ExecuteMsg::BtcHeaders {
-        headers: test_headers,
-    };
-
-    execute::<_, _, _, _, BabylonMsg>(&mut deps, env, info, execute_msg).unwrap();
-}
-
-#[test]
-fn btc_headers_query_works() {
-    let mut deps = setup();
-    let env = mock_env();
-    let info = mock_info("anyone", &[]);
-
-    let test_headers = get_main_msg_test_headers();
-
-    let execute_msg = ExecuteMsg::BtcHeaders {
-        headers: test_headers.clone(),
-    };
-
-    execute::<_, _, _, _, BabylonMsg>(&mut deps, env.clone(), info, execute_msg).unwrap();
-
-    let query_msg = QueryMsg::BtcHeaders {
-        start_after: None,
-        limit: None,
-        reverse: None,
-    };
-    let res: BtcHeadersResponse = from_json(query(&mut deps, env, query_msg).unwrap()).unwrap();
-
-    assert_eq!(res.headers.len(), 10); // default limit
-    assert_eq!(
-        test_headers[..10],
-        res.headers.iter().map(Into::into).collect::<Vec<_>>()
-    );
-}
-*/
