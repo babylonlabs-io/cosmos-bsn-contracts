33d5cea97a8fab81eb62da0c40ddd7a0327b62b8
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2025-07-16 14:13:22 +0800

    fix(btc-light-client): improve integration test and CI (#211)
    I just noticed the integration test in btc-light-client is broken, this
    PR fixes it by moving it to the right location. The updated integration
    test now matches the other ones in the other contracts. ~~It also
    updates the `local-tests.yml` workflow to run all workspace tests,
    ensuring better test coverage~~.

---
+++ contracts/btc-light-client/tests/integration.rs
@@ -0,0 +1,41 @@
+use btc_light_client::msg::contract::InstantiateMsg;
+use cosmwasm_std::testing::message_info;
+use cosmwasm_std::{Addr, ContractResult, Response};
+use cosmwasm_vm::testing::{instantiate, mock_env, mock_instance};
+
+#[cfg(clippy)]
+static BTC_LIGHT_CLIENT_CONTRACT_WASM: &[u8] = &[];
+
+#[cfg(not(clippy))]
+static BTC_LIGHT_CLIENT_CONTRACT_WASM: &[u8] =
+    include_bytes!("../../../artifacts/btc_light_client.wasm");
+
+/// Wasm size limit: https://github.com/CosmWasm/wasmd/blob/main/x/wasm/types/validation.go#L24-L25
+const MAX_WASM_SIZE: usize = 1024 * 1024; // 1 MB
+
+const CREATOR: &str = "creator";
+
+#[test]
+fn wasm_size_limit_check() {
+    assert!(
+        BTC_LIGHT_CLIENT_CONTRACT_WASM.len() < MAX_WASM_SIZE,
+        "BTC light client contract wasm binary is too large: {} (target: {MAX_WASM_SIZE})",
+        BTC_LIGHT_CLIENT_CONTRACT_WASM.len(),
+    );
+}
+
+#[test]
+fn instantiate_works() {
+    let mut deps = mock_instance(BTC_LIGHT_CLIENT_CONTRACT_WASM, &[]);
+
+    let msg = InstantiateMsg {
+        network: btc_light_client::BitcoinNetwork::Regtest,
+        btc_confirmation_depth: 10,
+        checkpoint_finalization_timeout: 100,
+        initial_header: babylon_test_utils::initial_header(),
+    };
+    let info = message_info(&Addr::unchecked(CREATOR), &[]);
+    let res: ContractResult<Response> = instantiate(&mut deps, mock_env(), info, msg);
+    let msgs = res.unwrap().messages;
+    assert_eq!(0, msgs.len());
+}
