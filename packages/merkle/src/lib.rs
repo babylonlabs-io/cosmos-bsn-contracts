63e1909455a2d7563c9080abec4baeef01e4625a
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2025-07-29 21:14:12 +0800

    crypto: improve merkle package (#258)
    No substantial misalignment found. 
    
    The update primarily aligned the reference implementation with CometBFT
    v0.38.17, removed unused code, and applied minor cleanups and
    refinements. I was trying to port more Go tests, but that incurs
    introducing more code that is not required in production.
    
    Close #237

--- packages/merkle/src/lib.rs
+++ packages/merkle/src/lib.rs
@@ -1,6 +1,24 @@
-pub mod error;
-pub mod hash;
-pub mod proof;
-pub mod tree;
+//! A Rust-native implementation of the CometBFT Merkle library.
+//!
+//! This crate provides a direct translation of the Merkle implementation from
+//! [`cometbft/cometbft`](https://github.com/cometbft/cometbft/tree/v0.38.17/crypto/merkle),
+//! intended for compatibility and correctness when verifying Tendermint-style Merkle proofs.
+//!
+//! We opted to reimplement it in Rust due to the lack of a drop-in Merkle library
+//! that fully matches CometBFT's behavior and structure.
+//!
+//! **Note:** This crate is intended as a stopgap. Once a suitable external Merkle library
+//! is available that provides full compatibility with CometBFT’s Merkle proof format,
+//! we plan to replace this implementation.
+//!
+//! ## Exports
+//! - [`MerkleError`](self::error::MerkleError): Errors encountered during proof verification or tree operations.
+//! - [`Proof`](self::proof::Proof): Structure and logic for Merkle proofs.
 
-pub use proof::Proof;
+mod error;
+mod hash;
+mod proof;
+mod tree;
+
+pub use self::error::MerkleError;
+pub use self::proof::Proof;
--- packages/merkle/src/proof.rs
+++ packages/merkle/src/proof.rs
@@ -1,98 +1,43 @@
-/// Based on comebft@0.38.6/crypto/merkle/proof.go
-use sha2::{Digest, Sha256};
+//! https://github.com/cometbft/cometbft/blob/v0.38.17/crypto/merkle/proof.go
 
 use crate::error::MerkleError;
-use cosmwasm_schema::cw_serde;
-use cosmwasm_std::Binary;
-
 use crate::hash::{inner_hash, leaf_hash};
 use crate::tree::get_split_point;
+use cosmwasm_schema::cw_serde;
+use cosmwasm_std::Binary;
+use sha2::{Digest, Sha256};
 
-/// A `Proof` is a proof of a leaf's existence in a Merkle tree.
-///
-/// The convention for proofs is to include leaf hashes, but to
+// MaxAunts is the maximum number of aunts that can be included in a Proof.
+// This corresponds to a tree of size 2^100, which should be sufficient for all conceivable purposes.
+// This maximum helps prevent Denial-of-Service attacks by limitting the size of the proofs.
+const MAX_AUNTS: usize = 100;
+
+/// Proof represents a Merkle proof.
+/// NOTE: The convention for proofs is to include leaf hashes but to
 /// exclude the root hash.
 /// This convention is implemented across IAVL range proofs as well.
 /// Keep this consistent unless there's a very good reason to change
-/// everything.
-/// This affects the generalized proof system as well.
+/// everything.  This also affects the generalized proof system as
+/// well.
 ///
-/// Equivalent to / adapted from cometbft/crypto/merkle/proof.go.
+/// https://github.com/cometbft/cometbft/blob/d03254d3599b973f979314e6383b89fa1802e679/crypto/merkle/proof.go#L26
 #[cw_serde]
 pub struct Proof {
+    /// Total number of items.
     pub total: u64,
+    /// Index of item to prove.
     pub index: u64,
+    /// Hash of item value.
     pub leaf_hash: Binary,
+    /// Hashes from leaf's sibling to a root's child.
     pub aunts: Vec<Binary>,
 }
 
-impl From<&tendermint_proto::crypto::Proof> for Proof {
-    fn from(proof_proto: &tendermint_proto::crypto::Proof) -> Self {
-        // Outright reject negative values for robustness
-        assert!(proof_proto.total >= 0);
-        assert!(proof_proto.index >= 0);
-        Proof {
-            total: proof_proto.total as u64,
-            index: proof_proto.index as u64,
-            leaf_hash: proof_proto.leaf_hash.clone().into(),
-            aunts: proof_proto
-                .aunts
-                .iter()
-                .cloned()
-                .map(|aunt| aunt.into())
-                .collect(),
-        }
-    }
-}
-
-impl From<tendermint_proto::crypto::Proof> for Proof {
-    fn from(proof_proto: tendermint_proto::crypto::Proof) -> Self {
-        Proof::from(&proof_proto)
-    }
-}
-
 impl Proof {
-    pub const MAX_AUNTS: usize = 100;
-
-    /// Performs basic validation.
-    ///
-    /// NOTE: it expects the `leaf_hash` and the elements of `aunts` to be of size `HASH_SIZE`,
-    /// and it expects at most `MAX_AUNTS` elements in `aunts`.
-    ///
-    /// Adapted from cometbft/crypto/bft/proof.go
-    /// (https://github.com/cometbft/cometbft/blob/68e5e1b4e3bd342a653a73091a1af7cc5e88b86b/crypto/merkle/proof.go#L145)
-    pub fn validate_basic(&self) -> Result<(), MerkleError> {
-        if self.leaf_hash.len() != Sha256::output_size() {
-            return Err(MerkleError::generic_err(format!(
-                "Expected leaf_hash size to be {}, got {}",
-                Sha256::output_size(),
-                self.leaf_hash.len()
-            )));
-        }
-        if self.aunts.len() > Proof::MAX_AUNTS {
-            return Err(MerkleError::generic_err(format!(
-                "Expected no more than {} aunts, got {}",
-                Proof::MAX_AUNTS,
-                self.aunts.len()
-            )));
-        }
-        for (i, aunt_hash) in self.aunts.iter().enumerate() {
-            if aunt_hash.len() != Sha256::output_size() {
-                return Err(MerkleError::generic_err(format!(
-                    "Expected aunt #{} size to be {}, got {}",
-                    i,
-                    Sha256::output_size(),
-                    aunt_hash.len()
-                )));
-            }
-        }
-        Ok(())
-    }
-
     /// Verifies that the Proof proves the root hash.
     ///
-    /// Adapted from `cometbft/crypto/bft/proof.go`
-    pub fn verify(&self, root_hash: &[u8], leaf: &[u8]) -> Result<bool, MerkleError> {
+    /// https://github.com/cometbft/cometbft/blob/d03254d3599b973f979314e6383b89fa1802e679/crypto/merkle/proof.go#L52
+    pub fn verify(&self, root_hash: &[u8], leaf: &[u8]) -> Result<(), MerkleError> {
         if root_hash.is_empty() {
             return Err(MerkleError::generic_err(
                 "Invalid root hash: cannot be empty",
@@ -100,7 +45,6 @@ impl Proof {
         }
         self.validate_basic()?;
         let leaf_hash = leaf_hash(leaf);
-
         if self.leaf_hash != leaf_hash {
             return Err(MerkleError::generic_err(format!(
                 "Invalid leaf hash: wanted {:X?} got {:X?}",
@@ -113,9 +57,10 @@ impl Proof {
                 "Invalid root hash: wanted {root_hash:X?} got {computed_hash:X?}"
             )));
         }
-        Ok(true)
+        Ok(())
     }
 
+    /// Compute the root hash given a leaf hash. Panics in case of errors.
     fn compute_root_hash(&self) -> Result<Vec<u8>, MerkleError> {
         compute_hash_from_aunts(
             self.index,
@@ -128,13 +73,82 @@ impl Proof {
                 .collect::<Vec<_>>(),
         )
     }
+
+    /// Performs basic validation.
+    ///
+    /// NOTE: it expects the `leaf_hash` and the elements of `aunts` to be of size `HASH_SIZE`,
+    /// and it expects at most `MAX_AUNTS` elements in `aunts`.
+    ///
+    /// https://github.com/cometbft/cometbft/blob/d03254d3599b973f979314e6383b89fa1802e679/crypto/merkle/proof.go#L113
+    pub fn validate_basic(&self) -> Result<(), MerkleError> {
+        if self.leaf_hash.len() != Sha256::output_size() {
+            return Err(MerkleError::generic_err(format!(
+                "Expected leaf_hash size to be {}, got {}",
+                Sha256::output_size(),
+                self.leaf_hash.len()
+            )));
+        }
+        if self.aunts.len() > MAX_AUNTS {
+            return Err(MerkleError::generic_err(format!(
+                "Expected no more than {MAX_AUNTS} aunts, got {}",
+                self.aunts.len()
+            )));
+        }
+        for (i, aunt_hash) in self.aunts.iter().enumerate() {
+            if aunt_hash.len() != Sha256::output_size() {
+                return Err(MerkleError::generic_err(format!(
+                    "Expected aunt #{i} size to be {}, got {}",
+                    Sha256::output_size(),
+                    aunt_hash.len()
+                )));
+            }
+        }
+        Ok(())
+    }
+}
+
+impl TryFrom<&tendermint_proto::crypto::Proof> for Proof {
+    type Error = String;
+    fn try_from(proof_proto: &tendermint_proto::crypto::Proof) -> Result<Self, Self::Error> {
+        if proof_proto.total < 0 {
+            return Err(format!(
+                "Invalid total `{}`: must not be negative",
+                proof_proto.total
+            ));
+        }
+
+        if proof_proto.index < 0 {
+            return Err(format!(
+                "Invalid index `{}`: must not be negative",
+                proof_proto.index
+            ));
+        }
+        Ok(Self {
+            total: proof_proto.total as u64,
+            index: proof_proto.index as u64,
+            leaf_hash: proof_proto.leaf_hash.clone().into(),
+            aunts: proof_proto
+                .aunts
+                .iter()
+                .cloned()
+                .map(|aunt| aunt.into())
+                .collect(),
+        })
+    }
+}
+
+impl TryFrom<tendermint_proto::crypto::Proof> for Proof {
+    type Error = String;
+    fn try_from(proof_proto: tendermint_proto::crypto::Proof) -> Result<Self, Self::Error> {
+        Self::try_from(&proof_proto)
+    }
 }
 
 // Use the leaf hash and inner hashes to get the root Merkle hash.
 // If the length of the inner_hashes slice isn't exactly correct,
 // the result is nil.
 // Recursive impl.
-pub fn compute_hash_from_aunts(
+fn compute_hash_from_aunts(
     index: u64,
     total: u64,
     leaf_hash: &[u8],
@@ -146,9 +160,7 @@ pub fn compute_hash_from_aunts(
         )));
     }
     match total {
-        0 => Err(MerkleError::generic_err(
-            "Cannot call compute_hash_from_aunts() with 0 total",
-        )),
+        0 => unreachable!("Cannot call compute_hash_from_aunts() with 0 total"),
         1 => {
             if !inner_hashes.is_empty() {
                 return Err(MerkleError::generic_err("Unexpected inner hashes"));
@@ -223,7 +235,7 @@ mod tests {
             total: 0,
             index: 0,
             leaf_hash: vec![0; 32].into(),
-            aunts: vec![vec![0; 32].into(); Proof::MAX_AUNTS + 1],
+            aunts: vec![vec![0; 32].into(); MAX_AUNTS + 1],
         };
         assert_eq!(
             proof.validate_basic(),
@@ -318,7 +330,7 @@ mod tests {
             leaf_hash: leaf_hash.clone().into(),
             aunts: vec![],
         };
-        assert_eq!(proof.verify(&root_hash, leaf), Ok(true));
+        assert!(proof.verify(&root_hash, leaf).is_ok());
 
         // Good case with aunts
         let proof = Proof {
@@ -330,7 +342,7 @@ mod tests {
         let root_hash =
             compute_hash_from_aunts(0, 2, &leaf_hash, &[inner_hash(&leaf_hash, &leaf_hash)])
                 .unwrap();
-        assert_eq!(proof.verify(&root_hash, leaf), Ok(true));
+        assert!(proof.verify(&root_hash, leaf).is_ok());
 
         let proof = Proof {
             total: 2,
@@ -341,7 +353,7 @@ mod tests {
         let root_hash =
             compute_hash_from_aunts(1, 2, &leaf_hash, &[inner_hash(&leaf_hash, &leaf_hash)])
                 .unwrap();
-        assert_eq!(proof.verify(&root_hash, leaf), Ok(true));
+        assert!(proof.verify(&root_hash, leaf).is_ok());
 
         // Invalid proof
         let proof = Proof {
