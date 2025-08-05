c3ee6ef5287639d1fd5218735579d6708f527c3f
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2025-08-05 15:44:21 +0800

    Port `TestMsgCommitPubRandListValidateBasic()`

--- contracts/btc-finality/src/finality.rs
+++ contracts/btc-finality/src/finality.rs
@@ -951,6 +951,105 @@ mod tests {
         (0..len).map(|_| rng.gen()).collect()
     }
 
+    // Helper function to generate random BTC key pair
+    fn gen_random_btc_key_pair(rng: &mut StdRng) -> (Vec<u8>, Vec<u8>) {
+        let sk = gen_random_bytes(rng, 32);
+        let pk = gen_random_bytes(rng, 32);
+        (sk, pk)
+    }
+
+    struct MsgCommitPubRandTestCase {
+        name: &'static str,
+        msg_modifier: fn(&mut MsgCommitPubRand),
+        expected: Result<(), PubRandCommitError>,
+    }
+
+    // Helper function to generate random message
+    fn gen_random_msg_commit_pub_rand(
+        rng: &mut StdRng,
+        start_height: u64,
+        num_pub_rand: u64,
+    ) -> MsgCommitPubRand {
+        let (_, pk) = gen_random_btc_key_pair(rng);
+        let commitment = gen_random_bytes(rng, COMMITMENT_LENGTH_BYTES);
+        let sig = gen_random_bytes(rng, 64); // BIP340 signature length
+
+        MsgCommitPubRand {
+            fp_btc_pk_hex: hex::encode(&pk),
+            start_height,
+            num_pub_rand,
+            commitment,
+            sig,
+        }
+    }
+
+    // https://github.com/babylonlabs-io/babylon/blob/49972e2d3e35caf0a685c37e1f745c47b75bfc69/x/finality/types/msg_test.go#L85
+    #[test]
+    fn test_msg_commit_pub_rand_validate_basic() {
+        let mut rng = StdRng::seed_from_u64(1);
+
+        let test_cases = vec![
+            MsgCommitPubRandTestCase {
+                name: "valid message",
+                msg_modifier: |_msg| {
+                    // No modification needed for valid message
+                },
+                expected: Ok(()),
+            },
+            MsgCommitPubRandTestCase {
+                name: "invalid commitment size",
+                msg_modifier: |msg: &mut MsgCommitPubRand| {
+                    msg.commitment = b"too-short".to_vec();
+                },
+                expected: Err(PubRandCommitError::BadCommitmentLength(9)),
+            },
+            MsgCommitPubRandTestCase {
+                name: "empty FP BTC PubKey",
+                msg_modifier: |msg: &mut MsgCommitPubRand| {
+                    msg.fp_btc_pk_hex = Default::default();
+                },
+                expected: Err(PubRandCommitError::EmptyFpBtcPubKey),
+            },
+            MsgCommitPubRandTestCase {
+                name: "empty signature",
+                msg_modifier: |msg| {
+                    msg.sig = vec![];
+                },
+                expected: Err(PubRandCommitError::EmptySignature),
+            },
+        ];
+
+        for MsgCommitPubRandTestCase {
+            name,
+            msg_modifier,
+            expected,
+        } in test_cases
+        {
+            let start_height = rng.gen_range(1..10);
+            let num_pub_rand = rng.gen_range(1..100);
+            let mut msg = gen_random_msg_commit_pub_rand(&mut rng, start_height, num_pub_rand);
+
+            // Apply the test case modifier
+            msg_modifier(&mut msg);
+
+            // Validate the message
+            assert_eq!(msg.validate_basic(), expected, "Test case failed: {name}");
+        }
+
+        // overflow in block height
+        let start_height = rng.gen_range(1..10);
+        let num_pub_rand = rng.gen_range(1..100);
+        let mut msg = gen_random_msg_commit_pub_rand(&mut rng, start_height, num_pub_rand);
+        msg.num_pub_rand = 0;
+        assert_eq!(
+            msg.validate_basic(),
+            Err(PubRandCommitError::OverflowInBlockHeight(
+                start_height,
+                start_height
+            ))
+        );
+    }
+
     // https://github.com/babylonlabs-io/babylon/blob/49972e2d3e35caf0a685c37e1f745c47b75bfc69/x/finality/types/msg_test.go#L167
     #[test]
     fn test_msg_add_finality_sig_validate_basic() {
