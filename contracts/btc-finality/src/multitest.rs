7b99601cbf0836f48c834d0599880a966e83a563
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2025-08-12 17:23:53 +0800

    Duplicate `finality_signature_happy_path()` as `test_add_finality_sig()`

--- contracts/btc-finality/src/multitest.rs
+++ contracts/btc-finality/src/multitest.rs
@@ -195,6 +195,98 @@ fn commit_public_randomness_works() {
 }
 
 #[test]
+fn test_add_finality_sig() {
+    // Read public randomness commitment test data
+    let (pk_hex, pub_rand, pubrand_signature) = get_public_randomness_commitment();
+    let pub_rand_one = get_pub_rand_value();
+    // Read equivalent / consistent add finality signature test data
+    let add_finality_signature = get_add_finality_sig();
+    let proof = add_finality_signature.proof.unwrap();
+
+    let initial_height = pub_rand.start_height;
+    let initial_funds = &[coin(1_000_000, "TOKEN")];
+
+    let mut suite = SuiteBuilder::new()
+        .with_height(initial_height)
+        .with_funds(initial_funds)
+        .build();
+
+    // Register one FP
+    // NOTE: the test data ensures that pub rand commit / finality sig are
+    // signed by the 1st FP
+    let new_fp = create_new_finality_provider(1);
+
+    suite.register_finality_providers(&[new_fp]).unwrap();
+
+    // Add a delegation, so that the finality provider has some power
+    let mut del1 = get_derived_btc_delegation(1, &[1]);
+    del1.fp_btc_pk_list = vec![pk_hex.clone()];
+
+    suite.add_delegations(&[del1]).unwrap();
+
+    suite
+        .commit_public_randomness(&pk_hex, &pub_rand, &pubrand_signature)
+        .unwrap();
+
+    // Call the begin-block sudo handler(s), for completeness
+    let res = suite
+        .call_begin_block(&add_finality_signature.block_app_hash, initial_height + 1)
+        .unwrap();
+    assert_eq!(2, res.events.len());
+    assert_eq!(
+        res.events[0],
+        Event::new("sudo").add_attribute("_contract_address", BTC_FINALITY_CONTRACT_ADDR)
+    );
+    // Check the finality provider status change event
+    assert_eq!(
+        res.events[1],
+        Event::new("wasm-finality_provider_status_change")
+            .add_attribute("_contract_address", BTC_FINALITY_CONTRACT_ADDR)
+            .add_attribute("btc_pk", &pk_hex)
+            .add_attribute("new_state", "FINALITY_PROVIDER_STATUS_ACTIVE")
+    );
+
+    // Call the end-block sudo handler(s), so that the block is indexed in the store
+    let res = suite
+        .call_end_block(&add_finality_signature.block_app_hash, initial_height + 1)
+        .unwrap();
+    assert_eq!(2, res.events.len());
+    assert_eq!(
+        res.events[0],
+        Event::new("sudo").add_attribute("_contract_address", BTC_FINALITY_CONTRACT_ADDR)
+    );
+    assert_eq!(
+        res.events[1],
+        Event::new("wasm-index_block")
+            .add_attribute("_contract_address", BTC_FINALITY_CONTRACT_ADDR)
+            .add_attribute("module", "finality")
+            .add_attribute("last_height", (initial_height + 1).to_string())
+    );
+
+    // Submit a finality signature from that finality provider at height initial_height + 1
+    let finality_sig = add_finality_signature.finality_sig.to_vec();
+    suite
+        .submit_finality_signature(
+            &pk_hex,
+            initial_height + 1,
+            &pub_rand_one,
+            &proof,
+            &add_finality_signature.block_app_hash,
+            &finality_sig,
+        )
+        .unwrap();
+
+    // Query finality signature for that exact height
+    let sig = suite.get_finality_signature(&pk_hex, initial_height + 1);
+    assert_eq!(
+        sig,
+        FinalitySignatureResponse {
+            signature: finality_sig
+        }
+    );
+}
+
+#[test]
 fn finality_signature_happy_path() {
     // Read public randomness commitment test data
     let (pk_hex, pub_rand, pubrand_signature) = get_public_randomness_commitment();
@@ -759,3 +851,4 @@ fn offline_fps_are_jailed() {
     assert_eq!(active_fps.len(), 1);
     assert!(active_fps.contains_key(&new_fp1.btc_pk_hex));
 }
+
