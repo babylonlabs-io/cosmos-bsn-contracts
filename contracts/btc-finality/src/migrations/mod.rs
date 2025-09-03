970925f2662ed947cf06c61fb64138090d4b64db
Gurjot <gurjotsyd@gmail.com>
2025-09-03 12:08:24 -0400

    rm migration fix

--- contracts/btc-finality/src/migrations/mod.rs
+++ contracts/btc-finality/src/migrations/mod.rs
@@ -1,5 +1,3 @@
-pub mod next_height_fix;
 pub mod v1_0_0_rc_0_to_v1_0_0_rc_1;
 
-pub use next_height_fix::fix_next_height_corruption;
 pub use v1_0_0_rc_0_to_v1_0_0_rc_1::migrate_config as migrate_config_v1_0_0_rc_0_to_v1_0_0_rc_1;
